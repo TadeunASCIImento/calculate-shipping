3ced5cf122a6c37e6890a3f1d4b9406d33d231a5
TadeunASCIImento <tadeunascimento36@gmail.com>
2023-03-01 09:11:35 -0300

    [build] configuração classe main da aplicação.

--- src/main/java/com/rest/calculate/shipping/CalculateShippingApplication.java
+++ src/main/java/com/rest/calculate/shipping/CalculateShippingApplication.java
@@ -2,12 +2,19 @@ package com.rest.calculate.shipping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CalculateShippingApplication {
+public class CalculateShippingApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalculateShippingApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CalculateShippingApplication.class);
 	}
 
 }
