f6c0af89f992b57b738d3f10f41c49406f3aad3b
TadeunASCIImento <tadeunascimento36@gmail.com>
2023-03-01 20:54:21 -0300

    [feature] add classe CalculateShippingControllerConstants

---
+++ src/main/java/com/rest/calculate/shipping/controllers/constants/CalculateShippingControllerConstants.java
@@ -0,0 +1,12 @@
+package com.rest.calculate.shipping.controllers.constants;
+
+public class CalculateShippingControllerConstants {
+
+	// URLS
+	public static final String END_POINT_CALCULATE_SHIPPING = "/shipping";
+
+	// MESSAGES
+	public static final String MSG_INFO_ZIP_CODE_NOT_FOUND = "[INFO]: Zip code not found.";
+
+	public static final String MSG_ERROR_CALCULATING_SHIPPING = "[Error] Calculating shipping";
+}
