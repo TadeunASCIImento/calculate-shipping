8368b9451819b54bf731592b63940b440a72f052
TadeunASCIImento <tadeunascimento36@gmail.com>
2023-02-28 23:15:45 -0300

    [Initial] Iniciando o projeto

---
+++ src/main/java/com/rest/calculate/shipping/CalculateShippingApplication.java
@@ -0,0 +1,13 @@
+package com.rest.calculate.shipping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculateShippingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculateShippingApplication.class, args);
+	}
+
+}
