5726cf51e6e09352c4c4bd3578f58d352a388c5f
TadeunASCIImento <tadeunascimento36@gmail.com>
2023-03-01 10:08:13 -0300

    [feature] add classe Address objeto de retorno da api viacep.

---
+++ src/main/java/com/rest/calculate/shipping/bean/Address.java
@@ -0,0 +1,24 @@
+package com.rest.calculate.shipping.bean;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Address implements Serializable{
+
+	private static final long serialVersionUID = 152938413907408373L;
+
+	    private String cep;
+	    private String logradouro;
+	    private String complemento;
+	    private String bairro;
+	    private String localidade;
+	    private String uf;
+	    private Long ibge;
+	    private Long gia;
+	    private Integer ddd;
+	    private Long siafi;
+}
