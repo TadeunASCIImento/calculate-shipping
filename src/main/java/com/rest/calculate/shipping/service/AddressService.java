420dba8a20b66eb2b0a987c5389727381ef86bdf
TadeunASCIImento <tadeunascimento36@gmail.com>
2023-03-01 20:27:59 -0300

    [refactor] add método calculateShippingValueByAddress + permitindo a mascara do cep na requisição

--- src/main/java/com/rest/calculate/shipping/service/AddressService.java
+++ src/main/java/com/rest/calculate/shipping/service/AddressService.java
@@ -1,13 +1,22 @@
 package com.rest.calculate.shipping.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.rest.calculate.shipping.bean.Address;
 import com.rest.calculate.shipping.dto.AddressDTO;
+import com.rest.calculate.shipping.interfaces.CalculateShippingInterface;
+import com.rest.calculate.shipping.utils.AddressServiceUtil;
 
 @Service
 public class AddressService {
+
+	@Autowired
+	private CalculateShippingInterface calculate;
+
+	@Autowired
+	private AddressServiceUtil util;
 
 	private final String URL_BASE_API_VIA_CEP = "https://viacep.com.br/ws/";
 	private final String RETURN_TYPE_JSON = "json";
@@ -16,11 +25,20 @@ public class AddressService {
 		return new RestTemplate();
 	}
 
-	public AddressDTO getAddressByCep(String cep) {
+	public Address getAddressByCep(String cep) throws Exception {
 		final String URL_API_VIA_CEP_GET_ADDRESS_BY_CEP = URL_BASE_API_VIA_CEP
-				.concat("/" + cep + "/" + "/" + RETURN_TYPE_JSON);
-		Address address = template().getForObject(URL_API_VIA_CEP_GET_ADDRESS_BY_CEP, Address.class);
-		return new AddressDTO(address);
+				.concat("/" + util.removeMaskFromCEP(cep) + "/" + "/" + RETURN_TYPE_JSON);
+		try {
+			return template().getForObject(URL_API_VIA_CEP_GET_ADDRESS_BY_CEP, Address.class);
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	public AddressDTO calculateShippingValueByAddress(Address address) {
+		AddressDTO dto = new AddressDTO(address);
+		dto.setFrete(calculate.calculateShippingByAddress(address));
+		return dto;
 	}
 
 }
