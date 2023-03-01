8f69c8491dfbccd0b13cb9852616c1c81160e17a
TadeunASCIImento <tadeunascimento36@gmail.com>
2023-03-01 09:07:31 -0300

    [feature] add classe que representa o cep para consulta.

---
+++ src/main/java/com/rest/calculate/shipping/bean/Cep.java
@@ -0,0 +1,14 @@
+package com.rest.calculate.shipping.bean;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+
+@Getter
+public class Cep implements Serializable{
+	
+	private static final long serialVersionUID = -621486629409648629L;
+
+	private String cep;
+	
+}
