5130f501332f962656fa203069709532223050bc
TadeunASCIImento <tadeunascimento36@gmail.com>
2023-03-02 11:06:49 -0300

    [refactor] add metodos setters na classe Cep

--- src/main/java/com/rest/calculate/shipping/bean/Cep.java
+++ src/main/java/com/rest/calculate/shipping/bean/Cep.java
@@ -3,8 +3,10 @@ package com.rest.calculate.shipping.bean;
 import java.io.Serializable;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class Cep implements Serializable{
 	
 	private static final long serialVersionUID = -621486629409648629L;
