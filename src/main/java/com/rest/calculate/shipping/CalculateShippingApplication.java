5b084ea9de66cef805460e4b3a8975d98731eb36
TadeunASCIImento <tadeunascimento36@gmail.com>
2023-03-01 23:10:27 -0300

    [feature] Habilitando o Swagger no projeto

--- src/main/java/com/rest/calculate/shipping/CalculateShippingApplication.java
+++ src/main/java/com/rest/calculate/shipping/CalculateShippingApplication.java
@@ -5,13 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class CalculateShippingApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalculateShippingApplication.class, args);
 	}
-	
+
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(CalculateShippingApplication.class);
