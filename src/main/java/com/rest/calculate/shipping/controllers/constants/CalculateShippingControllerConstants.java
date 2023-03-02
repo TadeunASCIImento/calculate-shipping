c50c150ba87877a6070d9cd2e77d5fdc986881d0
TadeunASCIImento <tadeunascimento36@gmail.com>
2023-03-02 11:15:06 -0300

    [refactor] mensagens de retorno

--- src/main/java/com/rest/calculate/shipping/controllers/constants/CalculateShippingControllerConstants.java
+++ src/main/java/com/rest/calculate/shipping/controllers/constants/CalculateShippingControllerConstants.java
@@ -6,7 +6,7 @@ public class CalculateShippingControllerConstants {
 	public static final String END_POINT_CALCULATE_SHIPPING = "/shipping";
 
 	// MESSAGES
-	public static final String MSG_INFO_ZIP_CODE_NOT_FOUND = "[INFO]: Zip code not found.";
+	public static final String MSG_INFO_ZIP_CODE_NOT_FOUND = "Zip code not found.";
 
-	public static final String MSG_ERROR_CALCULATING_SHIPPING = "[Error] Calculating shipping";
+	public static final String MSG_ERROR_CALCULATING_SHIPPING = "Error calculating shipping";
 }
